f7d44b6080ad01600cf2476a22841b73ae693111
Misaela <randrianasolo.m@zurcher.edu.mg>
2022-12-08 23:27:09 +0300

    Ajout d'une méthode de test d'égalité dans la classe de test MoneyTest

--- tpTestUnitaire/src/fr/emse/test/MoneyTest.java
+++ tpTestUnitaire/src/fr/emse/test/MoneyTest.java
@@ -14,6 +14,18 @@ public class MoneyTest {
 		Money result = m12CHF.add(m14CHF); // exécution de la méthode testée
 		assertTrue(expected.equals(result)); // comparaison
 	}
+	
+	@Test
+	public void testEquals() {
+		 Money m12CHF= new Money(12, "CHF");
+		 Money m14CHF= new Money(14, "CHF");
+		 
+		 assertTrue(!m12CHF.equals(null));
+		 assertEquals(m12CHF, m12CHF);
+		 assertEquals(m12CHF, new Money(12, "CHF"));
+		 assertTrue(!m12CHF.equals(m14CHF));
+	}
+
 
 
 }
